53e27987ea52306637ad6b478bdc3d3009717ca7
Ofek-D <ofekdekel96@gmail.com>
2025-06-29 16:54:44 +0300

    email changed to outlook

--- Lilach/email/src/main/java/org/email/SendMail.java
+++ Lilach/email/src/main/java/org/email/SendMail.java
@@ -3,12 +3,7 @@ package org.email;
 import java.awt.*;
 import java.net.URL;
 import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -16,61 +11,42 @@ public class SendMail {
 
     public static void main(String[] args) {
 
-        // Recipient's email ID needs to be mentioned.
+        // כתובת הנמען
         String to = args[0];
 
-        // Sender's email ID needs to be mentioned
-        String from = "noreplylilach@gmail.com";
+        // כתובת שולח (Outlook)
+        String from = "notreply_lilach@outlook.com";
 
-        // Assuming you are sending email from through gmails smtp
-        String host = "smtp.gmail.com";
+        // הגדרות שרת ה-SMTP של Outlook
+        String host = "smtp.office365.com";
 
-        // Get system properties
         Properties properties = System.getProperties();
-
-        // Setup mail server
         properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", "465");
-        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.auth", "true");
 
-        // Get the Session object.// and pass username and password
+        // יצירת session עם אימות משתמש
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-
             protected PasswordAuthentication getPasswordAuthentication() {
-
-                return new PasswordAuthentication("noreplylilach@gmail.com", "wcjdlbashflbisva");
-
+                return new PasswordAuthentication("notreply_lilach@outlook.com", "lilach1!");
             }
-
         });
 
-
         try {
-            // Create a default MimeMessage object.
             MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
             message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-            // Set Subject: header field
             message.setSubject(args[2]);
-
-            // Now set the actual message
             message.setText(args[1]);
 
             System.out.println("sending... to " + to + " | subject: " + args[2]);
-            // Send message
             Transport.send(message);
-            System.out.println("Sent message successfully.... to " + to + " | subject: " + args[2]);
+            System.out.println("Sent message successfully....");
 
         } catch (MessagingException mex) {
             mex.printStackTrace();
         }
-
     }
 
     public static void openWebpage(String urlString) {
@@ -80,6 +56,4 @@ public class SendMail {
             e.printStackTrace();
         }
     }
-
-
 }
